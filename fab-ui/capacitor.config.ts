239217d810297c00c9079c1ea4dfa609db248596
Cnidarias <roessner.pascal@gmail.com>
2025-02-10 09:21:49 +0100

    🎉 FAB: Create the ionic project

---
+++ fab-ui/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'io.ionic.starter',
+    appName: 'fab-ui',
+    webDir: 'www',
+};
+
+export default config;
